db7e026afd997388de1d27580e3749051252f3cf
Viktor Ödman <vo222dq@student.lnu.se>
2020-05-13 09:24:12 +0200

    Initial commit

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -2,19 +2,19 @@ import { IS_LOGGED_IN, LOGOUT_SUCCESS, USER_TOKEN } from './types'
 
 
 export const isLoggedIn = () => async (dispatch) => {
-    let response = await fetch('http://localhost:5000/api/auth/isLoggedIn', {method: 'GET', credentials: 'include'})
+    let response = await fetch('/api/auth/isLoggedIn', {method: 'GET', credentials: 'include'})
     response = await response.json()
     dispatch({ type: IS_LOGGED_IN, payload: response.data})
 }
 
 export const logout = () => async (dispatch) => {
-    let response = await fetch('http://localhost:5000/api/auth/logout', {method: 'POST', credentials: 'include'})
+    let response = await fetch('/api/auth/logout', {method: 'POST', credentials: 'include'})
     response = await response.json()
     dispatch({ type: LOGOUT_SUCCESS, payload: response.data})
 }
 
 export const getToken = () => async (dispatch) => {
-    let response = await fetch('http://localhost:5000/api/auth/getToken', {method: 'GET', credentials: 'include'})
+    let response = await fetch('/api/auth/getToken', {method: 'GET', credentials: 'include'})
     response = await response.json()
     return response.data
 }
