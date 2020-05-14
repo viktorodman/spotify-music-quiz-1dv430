91c2120a2785711dc2971db4a5fae465f443e612
Viktor Ödman <vo222dq@student.lnu.se>
2020-05-14 10:44:37 +0200

    Fixing spotifyPlayer

--- client/src/components/SpotifyPlayer/SpotifyPlayer.js
+++ client/src/components/SpotifyPlayer/SpotifyPlayer.js
@@ -18,42 +18,57 @@ waitForSpotify = async () => {
     
     const { Player } = await this.waitForSpotify()
     
-    this.player = await new Player({
+    this.player = new Player({
       name: 'Quiz Player',
       getOAuthToken: cb => {
          cb(this.props.userToken) 
         }
     })
-
+  }
     // Error handling
-    this.player.addListener('initialization_error', ({ message }) => { console.error(message) })
+    /* this.player.addListener('initialization_error', ({ message }) => { console.error(message) })
     this.player.addListener('authentication_error', ({ message }) => { console.error(message) })
     this.player.addListener('account_error', ({ message }) => { console.error(message) })
-    this.player.addListener('playback_error', ({ message }) => { console.error(message) })
+    this.player.addListener('playback_error', ({ message }) => { console.error(message) }) */
 
     // Playback status updates
-    this.player.addListener('player_state_changed', state => { console.log(state) })
+    /* this.player.addListener('player_state_changed', state => { console.log(state) }) */
 
     // Ready
-    this.player.addListener('ready', ({ device_id }) => {
+    /* this.player.addListener('ready', ({ device_id }) => {
       console.log('Ready with Device ID', device_id)
       
       this.props.setDevice(device_id)
-  })
+  }) */
 
     // Not Ready
-    this.player.addListener('not_ready', ({ device_id }) => {
+   /*  this.player.addListener('not_ready', ({ device_id }) => {
       console.log('Device ID has gone offline', device_id)
-    })
+    }) */
 
     // Connect to the player!
     
-    await this.player.connect();
+    /* await this.player.connect();
+  } */
+
+  connectToPlayer() {
+    if (this.player) {
+      clearTimeout(this.connectToPlayerTimeout)
+    // add event listeners to player getting into "ready" state
+     this.player.addListener('ready', 
+      ({device_id}) => {
+        this.props.setDevice(device_id)
+      })
+      this.player.connect()
+    } else {
+        this.connectToPlayerTimeout = setTimeout(this.connectToPlayer.bind(this), 1000)
+    }
   }
 
 
   componentDidMount() {
-    this.createSpotifyPlayer()
+    this.createSpotifyPlayer().then(() => this.connectToPlayer())
+
   }
 
   
