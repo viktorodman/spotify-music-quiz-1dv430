fa0a2b6ca2417eb2d0704e1b58f79cfea6a65bb5
Viktor Ödman <vo222dq@student.lnu.se>
2020-05-14 12:15:06 +0200

    Fixing spotifyPlayer

--- client/src/components/SpotifyPlayer/SpotifyPlayer.js
+++ client/src/components/SpotifyPlayer/SpotifyPlayer.js
@@ -26,16 +26,16 @@ waitForSpotify = async () => {
     })
   }
     // Error handling
-    /* this.player.addListener('initialization_error', ({ message }) => { console.error(message) })
+   /*  this.player.addListener('initialization_error', ({ message }) => { console.error(message) })
     this.player.addListener('authentication_error', ({ message }) => { console.error(message) })
     this.player.addListener('account_error', ({ message }) => { console.error(message) })
     this.player.addListener('playback_error', ({ message }) => { console.error(message) }) */
 
     // Playback status updates
-    /* this.player.addListener('player_state_changed', state => { console.log(state) }) */
+   /*  this.player.addListener('player_state_changed', state => { console.log(state) }) */
 
     // Ready
-    /* this.player.addListener('ready', ({ device_id }) => {
+   /*  this.player.addListener('ready', ({ device_id }) => {
       console.log('Ready with Device ID', device_id)
       
       this.props.setDevice(device_id)
@@ -48,7 +48,7 @@ waitForSpotify = async () => {
 
     // Connect to the player!
     
-    /* await this.player.connect();
+   /*  await this.player.connect();
   } */
 
   connectToPlayer() {
@@ -68,6 +68,36 @@ waitForSpotify = async () => {
 
   componentDidMount() {
     this.createSpotifyPlayer().then(() => this.connectToPlayer())
+    /* window.onSpotifyWebPlaybackSDKReady = () => {
+      const token = this.props.userToken
+      const player = new Spotify.Player({
+        name: 'Web Playback SDK Quick Start Player',
+        getOAuthToken: cb => { cb(token); }
+      }); */
+    
+      // Error handling
+     /*  player.addListener('initialization_error', ({ message }) => { console.error(message) })
+      player.addListener('authentication_error', ({ message }) => { console.error(message) })
+      player.addListener('account_error', ({ message }) => { console.error(message) })
+      player.addListener('playback_error', ({ message }) => { console.error(message) })
+     */
+      // Playback status updates
+      /* player.addListener('player_state_changed', state => { console.log(state); })
+     */
+      // Ready
+      /* player.addListener('ready', ({ device_id }) => {
+        console.log('Ready with Device ID', device_id)
+        this.props.setDevice(device_id)
+      }) */
+    
+      // Not Ready
+     /*  player.addListener('not_ready', ({ device_id }) => {
+        console.log('Device ID has gone offline', device_id)
+      }) */
+    
+      // Connect to the player!
+      /* player.connect() */
+   /*  } */
 
   }
 
